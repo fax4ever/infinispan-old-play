302162c84848f9957b5afe6bb3c85905af8d18ce
fax4ever <fabiomassimo.ercoli@gmail.com>
2017-12-23 14:08:06 +0100

    new infinispan cluster counter demo project

---
+++ infinispan-cluster-counter/src/main/java/it/redhat/demo/producer/CacheManagerProducer.java
@@ -0,0 +1,52 @@
+package it.redhat.demo.producer;
+
+import org.infinispan.configuration.cache.Configuration;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.counter.api.Storage;
+import org.infinispan.counter.configuration.CounterManagerConfigurationBuilder;
+import org.infinispan.counter.configuration.Reliability;
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.transaction.LockingMode;
+import org.infinispan.transaction.TransactionMode;
+import org.infinispan.transaction.lookup.GenericTransactionManagerLookup;
+import org.infinispan.util.concurrent.IsolationLevel;
+
+/**
+ * @author Fabio Massimo Ercoli (C) 2017 Red Hat Inc.
+ */
+public class CacheManagerProducer {
+
+	public static final String COUNTER_NAME = "HIBERNATE_OGM_SEQUENCE";
+
+	public EmbeddedCacheManager produce() {
+
+		GlobalConfigurationBuilder globalBuilder = GlobalConfigurationBuilder.defaultClusteredBuilder();
+
+		globalBuilder.addModule( CounterManagerConfigurationBuilder.class )
+			.numOwner(2)
+			.reliability( Reliability.CONSISTENT )
+			.addStrongCounter()
+				.name( COUNTER_NAME )
+				.initialValue( 0 )
+				.storage( Storage.VOLATILE );
+
+		Configuration config = new ConfigurationBuilder()
+			.transaction()
+				.transactionMode( TransactionMode.TRANSACTIONAL )
+				.transactionManagerLookup( new GenericTransactionManagerLookup() )
+				.useSynchronization( false )
+				.lockingMode( LockingMode.PESSIMISTIC )
+			.clustering().hash().groups().enabled()
+			.locking()
+				.concurrencyLevel( 10 )
+				.useLockStriping( false )
+				.isolationLevel( IsolationLevel.REPEATABLE_READ )
+			.build();
+
+		return new DefaultCacheManager( globalBuilder.build(), config );
+
+	}
+
+}
---
+++ infinispan-cluster-counter/src/test/java/it/redhat/demo/test/ClusterCounterTest.java
@@ -0,0 +1,117 @@
+package it.redhat.demo.test;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import org.infinispan.counter.EmbeddedCounterManagerFactory;
+import org.infinispan.counter.api.CounterConfiguration;
+import org.infinispan.counter.api.CounterManager;
+import org.infinispan.counter.api.CounterType;
+import org.infinispan.counter.api.Storage;
+import org.infinispan.counter.api.StrongCounter;
+import org.infinispan.counter.exception.CounterException;
+import org.infinispan.manager.EmbeddedCacheManager;
+
+import it.redhat.demo.producer.CacheManagerProducer;
+
+import static it.redhat.demo.producer.CacheManagerProducer.COUNTER_NAME;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+/**
+ * @author Fabio Massimo Ercoli (C) 2017 Red Hat Inc.
+ */
+@RunWith(JUnit4.class)
+public class ClusterCounterTest {
+
+	private static EmbeddedCacheManager cacheManager;
+
+	@BeforeClass
+	public static void beforeClass() {
+
+		cacheManager = new CacheManagerProducer().produce();
+
+	}
+
+	@Test
+	public void test_name_ok() {
+
+		assertNotNull( cacheManager );
+		CounterManager counterManager = EmbeddedCounterManagerFactory.asCounterManager( cacheManager );
+		assertNotNull( counterManager );
+		StrongCounter strongCounter = counterManager.getStrongCounter( COUNTER_NAME );
+		assertNotNull( strongCounter );
+
+	}
+
+	@Test(expected = CounterException.class)
+	public void test_name_ko() {
+
+		CounterManager counterManager = EmbeddedCounterManagerFactory.asCounterManager( cacheManager );
+		counterManager.getStrongCounter( COUNTER_NAME + "_KO" );
+		fail();
+
+	}
+
+	@Test
+	public void test_generateId() throws Exception {
+
+		// given a strong counter
+		CounterManager counterManager = EmbeddedCounterManagerFactory.asCounterManager( cacheManager );
+		StrongCounter strongCounter = counterManager.getStrongCounter( COUNTER_NAME );
+
+		// when increment and get
+		Long newValue = strongCounter.incrementAndGet().get();
+		// then 1
+		assertEquals( 1l, newValue.longValue() );
+
+		// when increment and get
+		newValue = strongCounter.incrementAndGet().get();
+		// then 2
+		assertEquals( 2l, newValue.longValue() );
+
+		// when increment and get
+		newValue = strongCounter.incrementAndGet().get();
+		// then 3
+		assertEquals( 3l, newValue.longValue() );
+
+	}
+
+	@Test
+	public void test_runtimeCounter_generateId() throws Exception {
+
+		// given a strong counter
+		CounterManager counterManager = EmbeddedCounterManagerFactory.asCounterManager( cacheManager );
+
+		// adding a runtime counter
+		final String hibernate_ogm_runtime_counter = "HIBERNATE_OGM_RUNTIME_COUNTER";
+
+		counterManager.defineCounter( hibernate_ogm_runtime_counter,
+			CounterConfiguration.builder( CounterType.UNBOUNDED_STRONG )
+				.initialValue( 0 )
+				.storage( Storage.VOLATILE )
+			.build());
+
+		StrongCounter strongCounter = counterManager.getStrongCounter( hibernate_ogm_runtime_counter );
+
+		// when increment and get
+		Long newValue = strongCounter.incrementAndGet().get();
+		// then 1
+		assertEquals( 1l, newValue.longValue() );
+
+		// when increment and get
+		newValue = strongCounter.incrementAndGet().get();
+		// then 2
+		assertEquals( 2l, newValue.longValue() );
+
+		// when increment and get
+		newValue = strongCounter.incrementAndGet().get();
+		// then 3
+		assertEquals( 3l, newValue.longValue() );
+
+	}
+
+}
